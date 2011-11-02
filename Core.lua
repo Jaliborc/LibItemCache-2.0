614454857e2ef37d0f46ca2263c14c3406d2776d
Jaliborc <contact@jaliborc.com>
2011-11-02 14:31:41 +0000

    Git the licenses right :P

--- Core.lua
+++ Core.lua
@@ -1,20 +1,20 @@
 --[[
 Copyright 2011 João Cardoso
-LibItemCache-1.0 is distributed under the terms of the GNU General Public License (or the Lesser GPL).
-This file is part of LibItemSearch.
+LibItemCache is distributed under the terms of the GNU General Public License (or the Lesser GPL).
+This file is part of LibItemCache.
 
-LibItemCache-1.0 is free software: you can redistribute it and/or modify
+LibItemCache is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-LibItemCache-1.0 is distributed in the hope that it will be useful,
+LibItemCache is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with LibItemSearch. If not, see <http://www.gnu.org/licenses/>.
+along with LibItemCache. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
 local Lib = LibStub:NewLibrary('LibItemCache-1.0', 3)
